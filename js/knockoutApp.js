2bee29412c44a04f1c1261552b58465cb063f304
Christiaan Quyn <christianq010@gmai.com>
2017-01-30 12:41:57 +0530

    Added list of cats to click on, array cat model implemented on knockout.js,

--- js/knockoutApp.js
+++ js/knockoutApp.js
@@ -1,13 +1,74 @@
-var ViewModel = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('./img/cat_picture2.jpeg');
-    this.ImageAttribution = ko.observable('alt_text');
+var initialcats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: './img/cat_picture1.jpg',
+        ImageAttribution: '_bluh',
+        catNickNames: ['Fert', 'Gnarles', 'Dranic']
+    },
+    {
+        clickCount: 0,
+        name: 'Blabby',
+        imgSrc: './img/cat_picture3.jpeg',
+        ImageAttribution: '_fluh',
+        catNickNames: ['Gert']
+    },
+    {
+        clickCount: 0,
+        name: 'Flabby',
+        imgSrc: './img/cat_picture4.jpeg',
+        ImageAttribution: '_huh',
+        catNickNames: ['Murt']
+    },
+    {
+        clickCount: 0,
+        name: 'Habby',
+        imgSrc: './img/cat_picture5.jpeg',
+        ImageAttribution: '_muh',
+        catNickNames: ['Hert']
+    }
+]
 
+var cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.ImageAttribution = ko.observable(data.ImageAttribution);
+    this.catNickNames = ko.observableArray(data.catNickNames);
+
+
+    this.title = ko.computed(function () {
+        var title;
+        var clicks = this.clickCount();
+        if (clicks < 10){
+            title = 'Rookie';
+        } else if (clicks < 50){
+            title = 'Major';
+        }
+        return title;
+    }, this);
+
+}
+
+var ViewModel = function () {
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialcats.forEach(function (catItem) {
+        self.catList.push(new cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
+
+    this.setCat = function (clickedCat) {
+        self.currentCat(clickedCat);
+    }
+
 
 }
 
